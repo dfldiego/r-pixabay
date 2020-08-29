4536401ca8446ebe209b2af8a78c2e38ef32d023
dfldiego <dfl.diego92@gmail.com>
2020-08-29 16:08:44 -0300

    subiendo docs folder

---
+++ docs/precache-manifest.cdd15f9dcf120b4167629919d1d71eed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e74157a85a5fc52bf6110ec28c0d22e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "596b44a9c5dd407d38e1",
+    "url": "/static/js/2.57d3c449.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57d3c449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e79c98238d55ac1fcf5",
+    "url": "/static/js/main.7f49c748.chunk.js"
+  },
+  {
+    "revision": "d54fdc3452b242e5cbae",
+    "url": "/static/js/runtime-main.cc83ca70.js"
+  }
+]);
